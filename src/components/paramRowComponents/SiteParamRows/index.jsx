d52c36f1ecf58340e577a7606f69574ba4baf3bc
BCJdevelopment <bcj@bcjdevelopment.com>
2022-09-22 12:21:59 -0700

    site metadata

--- src/components/paramRowComponents/SiteParamRows/index.jsx
+++ src/components/paramRowComponents/SiteParamRows/index.jsx
@@ -16,6 +16,61 @@ const SiteItems = [
     },
 ]
 
+const SiteMetadataItems = [
+    {
+        parameter: 'region',
+        description: 'The region the restoration site is located in.'
+    },
+    {
+        parameter: 'province',
+        description: 'The province the restoration site is located in.'
+    },
+    {
+        parameter: 'city',
+        description: 'The city the restoration site is located in.',
+    },
+    {
+        parameter: 'partnerName',
+        description: 'The name of the partner that runs the restoration site.',
+    },
+    {
+        parameter: 'siteType',
+        description: 'The type of the restoration site.'
+    },
+    {
+        parameter: 'acreage',
+        description: 'How many acres the restoration site covers.',
+    },
+    {
+        parameter: 'hectaresToRestore',
+        description: 'The number of hectares to be restored.',
+    },
+    {
+        parameter: 'totalPlantQuantity',
+        description: 'The total number of plants being planted at the restoration site.',
+    },
+    {
+        parameter: 'season',
+        description: 'Season of planting for the restoration site.',
+    },
+    {
+        parameter: 'restorationTypes',
+        description: 'The types of restoration taking place at the site.',
+    },
+    {
+        parameter: 'siteStatus',
+        description: 'The planting status of the site.',
+    },
+    {
+        parameter: 'acresRestored',
+        description: 'The number of acres that have been restored by efforts at the site.',
+    },
+    {
+        parameter: 'treesPlantedPerDay',
+        description: 'Approximate number of trees planted per day at the site.',
+    }
+]
+
 const SiteParamRows = ({
     level = 0,
     ...props
@@ -46,6 +101,24 @@ const SiteParamRows = ({
                     description="URL to retrieve the map image."
                     level={level + 1}
                 />
+
+            </ResponseParamRow>
+
+            <ResponseParamRow
+                parameter="metadata"
+                description="Additional site metadata (some fields may not be present)."
+                level={level}
+                dataType="object"
+                collapsible
+            >
+
+                {SiteMetadataItems.map(siteMetadataItem => {
+                    return <ResponseParamRow
+                        key={siteMetadataItem.parameter}
+                        {...siteMetadataItem}
+                        level={level + 1}
+                    />
+                })}
 
             </ResponseParamRow>
 
